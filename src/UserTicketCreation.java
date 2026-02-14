623c2e62fb482416a5c4e0c56073a07f059bf1cc
kanishkaa <kanishkaas2007@gmail.com>
2026-02-15 00:09:36 +0530

    init commit

--- src/UserTicketCreation.java
+++ src/UserTicketCreation.java
@@ -3,20 +3,43 @@ import java.util.Scanner;
 public class UserTicketCreation {
     public static void main(String[] args){
         Scanner n = new Scanner ( System.in );
+        int TicketId = 1 ;
+        char A;
+        do {
 
-        System.out.print ( "ENTER YOUR NAME: " );
-        String name = n.nextLine();
-        System.out.print ( " ENTER THE ISSUE THAT YOU ARE FACING: ");
-        String issue = n.nextLine();
-        System.out.print ( " ENTER THE CATEGORY OF THE ISSUE: ");
-        String category = n.nextLine();
+            System.out.print(" ENTER YOUR NAME: ");
+            String name = n.nextLine();
 
-        int TicketId = 001;
+            System.out.print(" ENTER THE ISSUE THAT YOU ARE FACING: ");
+            String issue = n.nextLine();
 
-        System.out.print( " NAME : " +name);
-        System.out.print( " STATED ISSUE : " +issue);
-        System.out.print( " UNDER THE CATEGORY : " +category);
-        System.out.print( " ISSUED TICKET ID : " +TicketId);
+            System.out.println(" ENTER THE CATEGORY OF THE ISSUE : ");
+            System.out.println(" 1. Technical Issue ");
+            System.out.println(" 2. Login Issue ");
+            System.out.println(" 3. Service Request ");
+            System.out.println(" 4. Transaction Issue ");
+            System.out.println(" 5. Other ");
+            //String category = n.nextLine();
 
+            System.out.print(" Enter Your Choice (1-5) ");
+            int choice = n.nextInt();
+
+
+            // int TicketId = 1;
+            //char A;
+
+            System.out.println(" Ticket Created ");
+            System.out.println(" NAME : " + name);
+            System.out.println(" STATED ISSUE : " + issue);
+            //System.out.print( " UNDER THE CATEGORY : " +category);
+            System.out.println(" ISSUED TICKET ID : " + TicketId);
+
+            TicketId++;
+            System.out.println("Do you want to Add Another Ticket? (y/n) ");
+            A = n.next().charAt(0);
+            n.nextLine();
+        }
+        while ( A == 'y' || A == 'Y');
+        System.out.println ("Thanks for your Report. The issue will be sorted");
     }
 }
